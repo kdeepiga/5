a1f87320420476f53ce88125e85a84a9d065f979
kdeepiga <kdeepigaece@gmail.com>
2017-07-07 11:09:22 +0530

    Add files via upload

---
+++ PowerOfANumber.java
@@ -0,0 +1,11 @@
+package guvi;
+import java.util.Scanner;
+public class PowerOfANumber {
+public static void main(String args[]){
+	int n;
+	Scanner s=new Scanner(System.in);
+	n=s.nextInt();
+	int count=0;
+	for()
+}
+}
